845fb72b31f9d3b8d706cb644c00efc5f96567a9
Ignaxavier <70979777+Ignaxavier@users.noreply.github.com>
2022-06-05 20:16:40 -0300

    Pasajeros

--- Assets/Scripts/Passeger/PassagersSkinsManager.cs
+++ Assets/Scripts/Passeger/PassagersSkinsManager.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PassagersSkinsManager : Passenger
+public class PassagersSkinsManager : MonoBehaviour
 {
     public static PassagersSkinsManager instance;
     [SerializeField] public List<Sprite> _allSkins = new List<Sprite>();
